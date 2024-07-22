fa1f6b578b022740bc68f25510922a543dc9cef1
Seth Ek <seth.ek@veteransunited.com>
2024-07-22 11:49:40 -0500

    yeah

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,14 +13,26 @@ func healthCheck(c *gin.Context) {
 	})
 }
 
-func main() {
+func getAlerts(c *gin.Context) {
+	url := "https://api.weather.gov/alerts/active/area/MO"
 
-	data := Data{
-		Number: 20,
+	// Make the request to get weather alerts
+	response, error := http.Get(url)
+	if error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": error.Error()})
+		return
 	}
-	fmt.Printf("%d\n", data.Number)
 
+	// Read body of response
+	body, error := io.ReadAll(response.Body)
+	if error == nil {
+		c.JSON(http.StatusOK, gin.H{"data": string(body)})
+	}
+}
+
+func main() {
 	router := gin.Default()
 	router.GET("/health", healthCheck)
+	router.GET("/alerts", getAlerts)
 	router.Run() // listen and serve on 0.0.0.0:8080
 }

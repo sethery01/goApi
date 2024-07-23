d3e2d7f555f1f48f6faed40a21ff54dfaab688d1
Seth Ek <seth.ek@veteransunited.com>
2024-07-23 09:27:26 -0500

    Pulled template

--- main.go
+++ main.go
@@ -1,38 +1,22 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
-func healthCheck(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "HEALTH OKAY",
-	})
-}
-
-func getAlerts(c *gin.Context) {
-	url := "https://api.weather.gov/alerts/active/area/MO"
-
-	// Make the request to get weather alerts
-	response, error := http.Get(url)
-	if error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": error.Error()})
-		return
-	}
-
-	// Read body of response
-	body, error := io.ReadAll(response.Body)
-	if error == nil {
-		c.JSON(http.StatusOK, gin.H{"data": string(body)})
-	}
-}
-
 func main() {
-	router := gin.Default()
-	router.GET("/health", healthCheck)
-	router.GET("/alerts", getAlerts)
-	router.Run() // listen and serve on 0.0.0.0:8080
+
+	// Kubernetes health route.
+	http.HandleFunc("/livez", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Healthy\n")
+	})
+
+	// Kubernetes ready route.
+	http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Ready\n")
+	})
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }

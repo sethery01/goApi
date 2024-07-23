2d78122f612d0790be2bdf12a3fb2f7b935a3b4d
Seth Ek <seth.ek@veteransunited.com>
2024-07-23 09:48:05 -0500

    Awesome code was made

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -17,6 +18,25 @@ func main() {
 	http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Ready\n")
 	})
-	
+
+	// Get weather alerts
+	http.HandleFunc("/alerts", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "ALERT ALERT\n\n")
+
+		url := "https://api.weather.gov/alerts/active/area/MO"
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprint(w, err)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprint(w, err)
+		}
+
+		fmt.Fprint(w, string(body))
+	})
+
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }

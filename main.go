0b0a71577f65ae17e223d1c10f2baceb46342b96
Seth Ek <seth.ek@veteransunited.com>
2024-07-23 13:18:13 -0500

    created getAlerts func

--- main.go
+++ main.go
@@ -8,6 +8,38 @@ import (
 	"net/http"
 )
 
+func getAlerts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "WEATHER ALERTS!!!\n\n")
+
+	url := "https://api.weather.gov/alerts/active/area/MO"
+
+	// Make a Get request to NWS
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+
+	// Read in response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+
+	// Unmarshal response body into struct
+	var alerts Alerts
+	err = json.Unmarshal(body, &alerts)
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+
+	// Print weather alerts and instructions to page
+	for i := range alerts.Features {
+		fmt.Fprintf(w, "Alert[%d]: %s\n", i, alerts.Features[i].Properties.Headline)
+		fmt.Fprintf(w, "Instruction: %s\n\n", alerts.Features[i].Properties.Instruction)
+	}
+
+}
+
 func main() {
 
 	// Kubernetes health route.
@@ -21,37 +53,7 @@ func main() {
 	})
 
 	// Get weather alerts
-	http.HandleFunc("/alerts", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "WEATHER ALERTS!!!\n\n")
-
-		url := "https://api.weather.gov/alerts/active/area/MO"
-
-		// Make a Get request to NWS
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprint(w, err)
-		}
-
-		// Read in response body
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Fprint(w, err)
-		}
-
-		// Unmarshal response body into struct
-		var alerts Alerts
-		err = json.Unmarshal(body, &alerts)
-		if err != nil {
-			fmt.Fprint(w, err)
-		}
-
-		// Print weather alerts and instructions to page
-		for i := range alerts.Features {
-			fmt.Fprintf(w, "Alert[%d]: %s\n", i, alerts.Features[i].Properties.Headline)
-			fmt.Fprintf(w, "Instruction: %s\n\n", alerts.Features[i].Properties.Instruction)
-		}
-
-	})
+	http.HandleFunc("/alerts", getAlerts)
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
